4d1b429af25660de33e8955eadfcd6a5ea9590d4
Alex Chiu <alexchiu11@gmail.com>
2020-12-27 10:55:21 +0000

    intial setup 🌱

---
+++ pages/_slug.vue
@@ -0,0 +1,19 @@
+<template>
+  <article>
+    <nuxt-content :document="article" />
+  </article>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  async asyncData({ $content, params }) {
+    const article = await $content('/', params.slug).fetch()
+
+    return { article }
+  },
+})
+</script>
+
+<style scoped></style>
