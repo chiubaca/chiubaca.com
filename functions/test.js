a7b5b53bb5ed29cade31917f19e166993cfbd94e
Alex Chiu <alexchiu11@gmail.com>
2020-12-28 11:01:03 +0000

    testing if it is possible to proxy to github webhook via netlify function

---
+++ functions/test.js
@@ -0,0 +1,32 @@
+const axios = require('axios')
+
+// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+const handler = async () => {
+  try {
+    const url = 'https://api.github.com/repos/chiubaca/dev-blog/dispatches'
+    const data = {
+      event_type: 'netlify_webhook_test',
+    }
+    const options = {
+      headers: {
+        Accept: 'application/vnd.github.everest-preview+json',
+        Authorization: `token ${process.env.GITHUB_TOKEN}`,
+      },
+    }
+
+    const resp = await axios.post(url, data, options)
+    console.log('success ', resp)
+    return {
+      statusCode: 200,
+      body: 'web hook invoked',
+      // // more keys you can return:
+      // headers: { "headerName": "headerValue", ... },
+      // isBase64Encoded: true,
+    }
+  } catch (error) {
+    console.error(error)
+    return { statusCode: 500, body: 'problem invoking webhook' }
+  }
+}
+
+module.exports = { handler }
