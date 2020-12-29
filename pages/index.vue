fe88127fb98a123992b4295cd1f501066db882b7
Alex Chiu <alexchiu11@gmail.com>
2020-12-29 16:58:49 +0000

    removed listing drafts on home page

--- pages/index.vue
+++ pages/index.vue
@@ -3,15 +3,8 @@
     <h1>Alex Chiu Dev Blog</h1>
     <h2>Blogs</h2>
     <div v-for="(post, index) in posts" :key="index">
-      <nuxt-link :to="post.slug" no-prefetch>{{ post.slug }}</nuxt-link>
+      <nuxt-link :to="post.slug">{{ post.slug }}</nuxt-link>
       {{ post.title }} || {{ post.id }}
-    </div>
-
-    <h2>Drafts</h2>
-
-    <div v-for="(draft, index) in drafts" :key="index">
-      <nuxt-link :to="draft.slug" no-prefetch>{{ draft.slug }}</nuxt-link>
-      {{ draft.title }} || {{ draft.id }}
     </div>
   </div>
 </template>
@@ -28,18 +21,7 @@ export default Vue.extend({
       }
     )
 
-    const drafts: DevTo.Article[] = await $axios.$get(
-      'https://dev.to/api/articles/me/unpublished?page=1&per_page=1000',
-      {
-        headers: { 'api-key': process.env.DEVTO_KEY },
-      }
-    )
-
-    return { posts, drafts }
-  },
-
-  beforeMount() {
-    console.log('Hello!!')
+    return { posts }
   },
 })
 </script>
