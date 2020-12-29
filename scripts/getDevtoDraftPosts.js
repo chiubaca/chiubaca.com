f437b7a0a98c6edafb64ce3b0baa80d43f136c20
Alex Chiu <alexchiu11@gmail.com>
2020-12-29 13:39:17 +0000

    Can add new files to gitub repo , but cat update as we need the sha of the file that it wants to update/delete. Show stopper. Need another way to do this.

--- scripts/getDevtoDraftPosts.js
+++ scripts/getDevtoDraftPosts.js
@@ -11,18 +11,19 @@ const axios = require('axios')
 if (process.env.NODE_ENV === 'development') require('dotenv').config()
 
 async function main() {
-  const response = await axios.get(
-    'https://dev.to/api/articles/me/unpublished?page=1&per_page=1000',
-    {
-      headers: { 'api-key': process.env.DEVTO_KEY },
-    }
-  )
+  try {
+    const response = await axios.get(
+      'https://dev.to/api/articles/me/unpublished?page=1&per_page=1000',
+      {
+        headers: { 'api-key': process.env.DEVTO_KEY },
+      }
+    )
 
-  // In local dev mode, write the files to disk
-  if (process.env.NODE_ENV === 'development') {
-    const articles = response.data
-    articles.forEach((article) => {
-      const contentWithFrontMatter = `---
+    // In local dev mode, write the files to disk
+    if (process.env.NODE_ENV === 'development') {
+      const articles = response.data
+      articles.forEach((article) => {
+        const contentWithFrontMatter = `---
   title: ${article.title}
   test: this is a test
   draft: true
@@ -30,54 +31,58 @@ async function main() {
   
   ${article.body_markdown}
   `
-      fs.writeFile(
-        `./content/drafts/${article.slug}.md`, // path file will be created to
-        contentWithFrontMatter, // content of markdown
-        function (err) {
-          if (err) return console.log(err)
-          console.log('new file created')
-        }
-      )
+        fs.writeFile(
+          `./content/drafts/${article.slug}.md`, // path file will be created to
+          contentWithFrontMatter, // content of markdown
+          function (err) {
+            if (err) return console.log(err)
+            console.log('Dev Mode: new article written to disk')
+          }
+        )
+      })
+    }
+
+    // In "Prod" mode, the context will in a github action, so we need to
+    // use the github SDK to write the files to the right place instead
+    const { Octokit } = require('@octokit/rest')
+    const octokit = new Octokit({
+      auth: process.env.GITHUB_TOKEN,
     })
+
+    const articles = response.data
+    for (const article of articles) {
+      const contentWithFrontMatter = `---
+      title: ${article.title}
+  
+      test: this is a test
+      draft: true
+      ---
+  
+      ${article.body_markdown}`
+
+      const buffContent = Buffer.from(contentWithFrontMatter, 'utf-8')
+      const contentEncoded = buffContent.toString('base64')
+
+      await octokit.repos.createOrUpdateFileContents({
+        owner: 'chiubaca',
+        repo: 'dev-blog',
+        path: `content/drafts/${article.slug}.md`,
+        message: 'created by webhook',
+        content: contentEncoded,
+        committer: {
+          name: 'Alex Chiu',
+          email: 'alexchiu11@gmail.com',
+        },
+        author: {
+          name: 'chiubaca',
+          email: 'alexchiu11@gmail.com',
+        },
+      })
+      console.log('Prod Mode: new article added via SDK')
+    }
+  } catch (error) {
+    console.error('Something went wrong', error)
   }
-
-  // In "Prod" mode, the context will in a github action, so we need to
-  // use the github SDK to write the files to the right place instead
-  const { Octokit } = require('@octokit/rest')
-  const octokit = new Octokit({
-    auth: process.env.GH_KEY,
-  })
-
-  const articles = response.data
-  articles.forEach((article) => {
-    const contentWithFrontMatter = `---
-    title: ${article.title}
-
-    test: this is a test
-    draft: true
-    ---
-
-    ${article.body_markdown}`
-
-    const buffContent = Buffer.from(contentWithFrontMatter, 'utf-8')
-    const contentEncoded = buffContent.toString('base64')
-
-    octokit.repos.createOrUpdateFileContents({
-      owner: 'chiubaca',
-      repo: 'dev-blog',
-      path: `content/drafts/${article.slug}.md`,
-      message: `provisioned automatically by webhook`,
-      content: contentEncoded,
-      committer: {
-        name: 'Alex Chiu',
-        email: 'alexchiu11@gmail.com',
-      },
-      author: {
-        name: 'chiubaca',
-        email: 'alexchiu11@gmail.com',
-      },
-    })
-  })
 }
 
 main()
--- scripts/test.js
+++ scripts/test.js
@@ -1,1 +1,18 @@
-console.log("I want this to be invoked via a web hook")
+// console.log("I want this to be invoked via a web hook")
+
+const { Octokit } = require('@octokit/rest')
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN,
+})
+
+async function main() {
+  try {
+
+    // List all files
+  
+  } catch (err) {
+    console.error('Something went wrong', err)
+  }
+}
+
+main()
