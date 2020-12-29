7663b02f706927ab01802a2de5abe10de7c241a0
Alex Chiu <alexchiu11@gmail.com>
2020-12-29 15:57:45 +0000

    Show drafts

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,17 @@
 <template>
   <div class="">
     <h1>Alex Chiu Dev Blog</h1>
+    <h2>Blogs</h2>
     <div v-for="(post, index) in posts" :key="index">
       <nuxt-link :to="post.slug" no-prefetch>{{ post.slug }}</nuxt-link>
       {{ post.title }} || {{ post.id }}
+    </div>
+
+    <h2>Drafts</h2>
+
+    <div v-for="(draft, index) in drafts" :key="index">
+      <nuxt-link :to="draft.slug" no-prefetch>{{ draft.slug }}</nuxt-link>
+      {{ draft.title }} || {{ draft.id }}
     </div>
   </div>
 </template>
@@ -20,9 +28,14 @@ export default Vue.extend({
       }
     )
 
-    console.log('Got articels')
+    const drafts: DevTo.Article[] = await $axios.$get(
+      'https://dev.to/api/articles/me/unpublished?page=1&per_page=1000',
+      {
+        headers: { 'api-key': process.env.DEVTO_KEY },
+      }
+    )
 
-    return { posts }
+    return { posts, drafts }
   },
 
   beforeMount() {
