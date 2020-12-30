a93fda55c5a231e0bba610ee3c37c42daa6a0645
Alex Chiu <alexchiu11@gmail.com>
2020-12-30 10:12:23 +0000

    secret archive page

---
+++ pages/archive/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="">
+    <h1>Archived Posts</h1>
+
+    <div v-for="(post, index) in archivedPosts" :key="index">
+      <NuxtLink :to="`archive/${post.slug}`"> {{ post.title }}</NuxtLink>
+      {{ post.title }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  async asyncData({ $content }) {
+    const archivedPosts = await $content('archive').fetch()
+    return { archivedPosts }
+  },
+})
+</script>
+
+<style></style>
