1f716fbbb31bd90a2edcc9fa3de08bad4e81d27a
Alex Chiu <alexchiu11@gmail.com>
2020-12-30 10:39:44 +0000

    added water.css for nice quick styling

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,13 +4,19 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'Chiubaca Dev Blog',
+    title: 'Alex Chiu',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: 'https://cdn.jsdelivr.net/npm/water.css@2/out/water.css',
+      },
+    ],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
