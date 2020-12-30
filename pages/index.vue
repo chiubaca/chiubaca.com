6a5f66cbae3e9022982a85b40f1504f6bf39f129
Alex Chiu <alexchiu11@gmail.com>
2020-12-30 10:59:36 +0000

    tweaked sub headings

--- pages/index.vue
+++ pages/index.vue
@@ -4,14 +4,14 @@
     <h2>Web Developer / Learning In Public / Building For Fun</h2>
 
     <h2>Blogs</h2>
-    <p>(Musings About Life and Fitness)</p>
+    <p>(Musings about life, wellbeing and fitness)</p>
     <div class="article" v-for="(blog, index) in blogs" :key="index">
       <NuxtLink :to="`${blog.slug}`"> {{ blog.title }}</NuxtLink>
       <p>Posted on: {{ new Date(blog.date).toDateString() }}</p>
     </div>
 
     <h2>Recent Dev.to Articles</h2>
-    <p>(Web Dev and Programming related articles)</p>
+    <p>(Web development and ramblings about programming)</p>
     <div v-for="(article, index) in devToArticles" :key="index" class="article">
       <a :href="article.url">{{ article.title }} </a>
       <p>Posted on: {{ new Date(article.published_at).toDateString() }}</p>
