46debd12fe4621d8d75ded30ea765501645afc02
Alex Chiu <alexchiu11@gmail.com>
2020-12-30 09:36:41 +0000

    Moved work on processing dev.to artivle to WIP directory

---
+++ pages/wip/_slug.vue
@@ -0,0 +1,58 @@
+<template>
+  <article v-html="highlightedHTML.value"></article>
+</template>
+
+<script lang="ts">
+/**
+ * 
+ * Hooo boy it's a mess in here...
+ * Cant decide if I should be parsing the markdown or just use the body_html
+ * 
+ * using markdown I have more control but need to work out how to process the shortcode
+ * using the html everything has been processed but need to figure out how to use highlight.js to style the code blocks
+ * 
+ */
+
+import Vue from 'vue'
+// import unified from 'unified'
+// import markdown from 'remark-parse'
+// import remark2rehype from 'remark-rehype'
+// import format from 'rehype-format'
+// import html from 'rehype-stringify'
+// import report from 'vfile-reporter'
+import hljs from 'highlight.js'
+// import javascript from 'highlight.js/lib/languages/javascript'
+// import 'highlight.js/styles/github.css'
+// hljs.registerLanguage('javascript', javascript)
+
+export default Vue.extend({
+  async asyncData({ params, $axios }) {
+    const post: DevTo.ArticleBySlug = await $axios.$get(
+      'https://dev.to/api/articles/chiubaca/' + params.slug
+    )
+
+    // let parsedMD
+    // unified()
+    //   .use(markdown)
+    //   .use(remark2rehype)
+    //   .use(format)
+    //   .use(html)
+    //   .process(post.body_markdown, function (err, file) {
+    //     console.error(report(err || file))
+    //     // console.log(String(file))
+    //     parsedMD = String(file)
+    //   })
+
+    const highlightedHTML = hljs.highlightAuto(post.body_html)
+    console.log('Highlighes', highlightedHTML.value)
+
+    return {
+      // parsedMD,
+      post,
+      highlightedHTML,
+    }
+  },
+})
+</script>
+
+<style scoped></style>
---
+++ pages/wip/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="">
+    <h1>Alex Chiu Dev Blog</h1>
+    <h2>Blogs</h2>
+    <div v-for="(post, index) in posts" :key="index">
+      <nuxt-link :to="`wip/${post.slug}`">{{ post.slug }}</nuxt-link>
+      {{ post.title }} || {{ post.id }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  async asyncData({ $axios }) {
+    const posts: DevTo.Article[] = await $axios.$get(
+      'https://dev.to/api/articles/me/published?page=1&per_page=1000',
+      {
+        headers: { 'api-key': process.env.DEVTO_KEY },
+      }
+    )
+
+    return { posts }
+  },
+})
+</script>
+
+<style></style>
