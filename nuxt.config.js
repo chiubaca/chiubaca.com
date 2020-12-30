a434f2cb2f0d2dd2e4959782167dde730c6fdd11
Alex Chiu <alexchiu11@gmail.com>
2020-12-30 18:23:35 +0000

    Added light and dark mode powered by css variables

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,17 +10,14 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      {
-        rel: 'stylesheet',
-        href: 'https://cdn.jsdelivr.net/npm/water.css@2/out/water.css',
-      },
-    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    bodyAttrs: {
+      'data-theme': 'light',
+    },
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [],
+  css: ['@/assets/styles/main.scss'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: ['plugins/preview.client.js'],
