208479acf7cda4db05ab0686e08abb1b1b1ce2c7
Alex Chiu <alexchiu11@gmail.com>
2020-12-30 09:49:29 +0000

    Redirect to dev.to posts for now

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="">
-    <h1>Alex Chiu Dev Blog</h1>
-    <h2>Blogs</h2>
+    <h1>Alex Chiu</h1>
+
+    <h2>Recent Dev.to Articles</h2>
     <div v-for="(post, index) in posts" :key="index">
-      <nuxt-link :to="post.slug">{{ post.slug }}</nuxt-link>
-      {{ post.title }} || {{ post.id }}
+      <p>{{ Date(post.published_at) }}</p>
+      <a :href="post.url">{{ post.title }} </a>
     </div>
   </div>
 </template>
@@ -15,7 +16,7 @@ import Vue from 'vue'
 export default Vue.extend({
   async asyncData({ $axios }) {
     const posts: DevTo.Article[] = await $axios.$get(
-      'https://dev.to/api/articles/me/published?page=1&per_page=1000',
+      'https://dev.to/api/articles/me/published?page=1&per_page=8',
       {
         headers: { 'api-key': process.env.DEVTO_KEY },
       }
