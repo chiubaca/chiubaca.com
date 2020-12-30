6d716daf097a44ba784a1ebc59e531649edfa611
Alex Chiu <alexchiu11@gmail.com>
2020-12-30 10:49:19 +0000

    added dir for life blogs

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,19 @@
+<template>
+  <article>
+    <nuxt-content :document="blog" />
+  </article>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  async asyncData({ $content, params }) {
+    const blog = await $content('blog', params.slug).fetch()
+
+    return { blog }
+  },
+})
+</script>
+
+<style scoped></style>
---
+++ pages/blog/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>My Blogs</h1>
+    <div v-for="(blog, index) in blogs" :key="index">
+      <NuxtLink :to="`blog/${blog.slug}`"> {{ blog.title }}</NuxtLink>
+      <p>Posted on: {{ blog.date }}</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  async asyncData({ $content }) {
+    const blogs = await $content('blog').fetch()
+    return { blogs }
+  },
+})
+</script>
+
+<style></style>
