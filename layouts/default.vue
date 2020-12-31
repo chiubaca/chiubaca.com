e9513b1de1c4c19cc1aaf72c14d782bc8bc46431
Alex Chiu <alexchiu11@gmail.com>
2020-12-31 14:01:09 +0000

    no need to import components

--- layouts/default.vue
+++ layouts/default.vue
@@ -14,12 +14,9 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import ThemeToggle from '@/components/ThemeToggle.vue'
+
 
 export default Vue.extend({
-  components: {
-    ThemeToggle,
-  },
 })
 </script>
 
