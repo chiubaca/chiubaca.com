51f049cf93a6105e0e54f4d66e51da2245ddd8d1
Alex Chiu <alexchiu11@gmail.com>
2020-12-31 15:25:30 +0000

    bullet seperators

--- components/Links.vue
+++ components/Links.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <div class="footer">
-      <a href="https://twitter.com/chiubaca">twitter</a> .
-      <a href="https://github.com/chiubaca">github</a> .
-      <a href="https://dev.to/chiubaca">dev.to</a> .
-      <a href="https://www.linkedin.com/">linkedin</a> .
-      <a href="mailto:alexchiu11@gmail.com">email</a> .
-      <a href="https://journal.chiubaca.com">journal</a> .
+      <a href="https://twitter.com/chiubaca">twitter</a> •
+      <a href="https://github.com/chiubaca">github</a> •
+      <a href="https://dev.to/chiubaca">dev.to</a> •
+      <a href="https://www.linkedin.com/">linkedin</a> •
+      <a href="mailto:alexchiu11@gmail.com">email</a> •
+      <a href="https://journal.chiubaca.com">journal</a> •
       <a href="https://chiubaca.carrd.co/">card</a>
     </div>
   </div>
