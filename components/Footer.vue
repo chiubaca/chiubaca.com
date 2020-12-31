7c07f67425042d4e7ed48fca146da655d7e3b29b
Alex Chiu <alexchiu11@gmail.com>
2020-12-31 14:59:31 +0000

    sementic footer + tidy up

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <hr />
-    <footer class="footer">
+    <div class="footer">
       <a href="https://twitter.com/chiubaca">twitter</a> .
       <a href="https://github.com/chiubaca">github</a> .
       <a href="https://dev.to/chiubaca">dev.to</a> .
@@ -9,14 +9,13 @@
       <a href="mailto:alexchiu11@gmail.com">email</a> .
       <a href="https://journal.chiubaca.com">journal</a> .
       <a href="https://chiubaca.carrd.co/">card</a>
-    </footer>
+    </div>
   </div>
 </template>
 
 <style scoped>
 .footer {
   display: flex;
-
   justify-content: space-evenly;
   flex-flow: wrap;
 }
