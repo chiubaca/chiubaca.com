359b8ce32f4bb768cd2b672537190c2bf50a3fcd
Alex Chiu <alexchiu11@gmail.com>
2021-01-01 15:23:12 +0000

    Display blog for individual nodes

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,33 +1,38 @@
 <template>
-  <Layout>
-
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
-
-    <h1>Hello, world!</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
-
+  <Layout class="blog">
+    <h1>Archived Blogs</h1>
+    <div v-for="blog in $page.archivedBlogs.edges" :key="blog.node.id">
+      <g-link :to="blog.node.path">{{ blog.node.title }}</g-link>
+    </div>
   </Layout>
 </template>
+
+<page-query>
+  query  {
+    archivedBlogs: allArchivedBlogs{
+      edges {
+        node {
+          id
+          title
+          date (format: "D MMMM, YYYY")
+          categories
+          path
+        }
+      }
+    }
+  }
+</page-query>
 
 <script>
 export default {
   metaInfo: {
-    title: 'Hello, world!'
-  }
-}
+    title: "Blog",
+  },
+};
 </script>
 
-<style>
-.home-links a {
-  margin-right: 1rem;
+<style scoped>
+.blog > ul {
+  list-style: none;
 }
 </style>
---
+++ src/templates/ArchivedBlogs.vue
@@ -0,0 +1,73 @@
+<template>
+  <Layout>
+    <div class="article">
+      <h1>{{ $page.post.title }}</h1>
+      <span>{{ $page.post.date }}</span>
+
+      <div class="content" v-html="$page.post.content" />
+    </div>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo() {
+    return {
+      title: this.$page.blogPost.title
+    };
+  }
+};
+</script>
+
+<page-query>
+  query archivedBlogs ($path: String!) {
+    post: archivedBlogs (path: $path) {
+      title
+      date
+      content
+    }
+  }
+</page-query>
+
+<style>
+/* .header {
+    margin-bottom: 70px;
+  } */
+.article {
+  margin-top: 15px;
+}
+.article h1 {
+  font-size: 40px;
+}
+.article img {
+  width: 100%;
+  border-radius: 5px;
+}
+.article a {
+  color: #4dba87;
+  text-decoration: underline;
+}
+.article a:hover {
+  text-decoration: none;
+}
+.article span {
+  font-size: 80%;
+  margin-bottom: 20px;
+}
+.article ol,
+.article ul {
+  list-style-position: outside;
+}
+.article ul {
+  list-style: disc;
+  padding-left: 20px;
+}
+.article .content p:first-child {
+  margin-top: 15px;
+}
+.article .content p {
+  margin-top: 0;
+  margin-bottom: 10px;
+  line-height: 1.5;
+}
+</style>
