52d43b45bc5cdd6c5388117ee7d6832e32db56ac
Alex Chiu <alexchiu11@gmail.com>
2021-01-01 10:50:17 +0000

    show md instead

--- pages/wip/_slug.vue
+++ pages/wip/_slug.vue
@@ -1,5 +1,5 @@
 <template>
-  <article v-html="post.body_html"></article>
+  <article v-html="parsedMD"></article>
 </template>
 
 <script lang="ts">
@@ -14,15 +14,18 @@
  */
 
 import Vue from 'vue'
-// import unified from 'unified'
-// import markdown from 'remark-parse'
-// import remark2rehype from 'remark-rehype'
-// import format from 'rehype-format'
-// import html from 'rehype-stringify'
-// import report from 'vfile-reporter'
-import hljs from 'highlight.js'
+import unified from 'unified'
+import markdown from 'remark-parse'
+import remark2rehype from 'remark-rehype'
+import format from 'rehype-format'
+import html from 'rehype-stringify'
+import report from 'vfile-reporter'
+// import hljs from 'highlight.js'
+// import 'highlight.js/styles/default.css'
 // import javascript from 'highlight.js/lib/languages/javascript'
 // import 'highlight.js/styles/github.css'
+// hljs.registerLanguage('javascript', javascript)
+// import javascript from 'highlight.js/lib/languages/javascript'
 // hljs.registerLanguage('javascript', javascript)
 
 export default Vue.extend({
@@ -31,28 +34,31 @@ export default Vue.extend({
       'https://dev.to/api/articles/chiubaca/' + params.slug
     )
 
-    // let parsedMD
-    // unified()
-    //   .use(markdown)
-    //   .use(remark2rehype)
-    //   .use(format)
-    //   .use(html)
-    //   .process(post.body_markdown, function (err, file) {
-    //     console.error(report(err || file))
-    //     // console.log(String(file))
-    //     parsedMD = String(file)
-    //   })
+    let parsedMD
+    unified()
+      .use(markdown)
+      .use(remark2rehype)
+      .use(format)
+      .use(html)
+      .process(post.body_markdown, function (err, file) {
+        console.error(report(err || file))
+        // console.log(String(file))
+        parsedMD = String(file)
+      })
 
-    const highlightedHTML = hljs.highlightAuto(post.body_html)
-    console.log('Highlighes', highlightedHTML.value)
+    // const highlightedHTML = hljs.highlightAuto(post.body_html)
+    // console.log('Highlighes', highlightedHTML.value)
 
     return {
-      // parsedMD,
+      parsedMD,
       post,
-      highlightedHTML,
+      // highlightedHTML,
     }
-  },
+  }
+
 })
 </script>
 
-<style scoped></style>
+<style scoped>
+
+</style>
