748c505e45ad2f573dea94da28ddb2d2eef9821d
Alex Chiu <alexchiu11@gmail.com>
2021-01-04 19:07:21 +0000

    updated page query to work with my plugin

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,13 +3,13 @@
 
     <h1>👨‍💻 Dev.to Blogs</h1>
         <div v-for="blog in $page.devToBlogs.edges" :key="blog.node.id">
-        <g-link :to="blog.node.slug">{{ blog.node.title }}</g-link>
+        <g-link :to="blog.node.path">{{ blog.node.title }}</g-link>
     </div>
 
 
     <h1>Archived Blogs</h1>
     <div v-for="blog in $page.archivedBlogs.edges" :key="blog.node.id">
-      <g-link :to="blog.node.path">{{ blog.node.title }}</g-link>
+      <g-link :to="blog.node.path">{{ blog.node.path }}</g-link>
     </div>
   </Layout>
 </template>
@@ -32,7 +32,7 @@
         node {
           id
           title
-          slug
+          path
         }
       }
     }
