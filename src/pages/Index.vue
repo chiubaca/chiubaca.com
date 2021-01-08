cc785f08067bc448629f3ea1c9bbb43568c7cf0d
Alex Chiu <alexchiu11@gmail.com>
2021-01-08 18:27:36 +0000

    Seperate out devjournal, link back to dev.to

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,13 +1,19 @@
 <template>
   <Layout>
     <h1>👨‍💻 Dev Blogs</h1>
-    <p> Web development and ramblings about programming</p>
+    <p>Web development and ramblings about programming</p>
     <div v-for="blog in $page.articles.edges" :key="blog.node.id">
       <g-link :to="blog.node.path">{{ blog.node.title }}</g-link>
     </div>
 
-    <h1>Archived Blogs</h1>
-    <p> Old posts. Awkward </p>
+    <h1>📘 Dev Journal</h1>
+    <p>Stories of success and mishaps whilst working on side projects</p>
+    <div v-for="blog in $page.devDiary.edges" :key="blog.node.id">
+      <a :href="blog.node.url">{{ blog.node.title }}</a>
+    </div>
+
+    <h1>🗄️ Archive</h1>
+    <p>Writings from my old website</p>
     <div v-for="(post, index) in $page.archivedBlogs.edges" :key="index">
       <g-link :to="post.node.path">{{ post.node.title }}</g-link>
     </div>
@@ -16,7 +22,7 @@
 
 <page-query>
   query {
-    archivedBlogs: allArchivedBlogs{
+    archivedBlogs: allArchivedBlogs(order: ASC){
       edges {
         node {
           id
@@ -27,11 +33,19 @@
         }
       }
     }
-    articles: allDevToArticles{
+    articles: allDevToArticles(filter: {tags: {containsNone: ["devjournal"] }} order: ASC){
       edges {
         node{
           title
           path
+        }
+      }
+    }
+    devDiary: allDevToArticles(filter: {tags: {contains: ["devjournal"] }} order: ASC){
+      edges {
+        node{
+          title
+          url
         }
       }
     }
