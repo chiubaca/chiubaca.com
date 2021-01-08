741150f54e186e6ed4c8ee88b260026d677bd3a1
Alex Chiu <alexchiu11@gmail.com>
2021-01-08 20:46:23 +0000

    blog timestamp and styling tweaks

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,21 +1,24 @@
 <template>
   <Layout>
     <h1>👨‍💻 Dev Blogs</h1>
-    <p>Web development and ramblings about programming</p>
-    <div v-for="blog in $page.articles.edges" :key="blog.node.id">
+    <p class="subtext">Web development and ramblings about programming</p>
+    <div class="post" v-for="blog in $page.articles.edges" :key="blog.node.id">
       <g-link :to="blog.node.path">{{ blog.node.title }}</g-link>
+      <p class="timestamp">posted on : {{ new Date(blog.node.published_at).toDateString() }}</p>
     </div>
 
     <h1>📘 Dev Journal</h1>
     <p>Stories of success and mishaps whilst working on side projects</p>
-    <div v-for="blog in $page.devDiary.edges" :key="blog.node.id">
+    <div class="post" v-for="blog in $page.devDiary.edges" :key="blog.node.id">
       <a :href="blog.node.url">{{ blog.node.title }}</a>
+      <p class="timestamp">posted on : {{ new Date(blog.node.published_at).toDateString()}}</p>
     </div>
 
     <h1>🗄️ Archive</h1>
     <p>Writings from my old website</p>
-    <div v-for="(post, index) in $page.archivedBlogs.edges" :key="index">
+    <div class="post" v-for="(post, index) in $page.archivedBlogs.edges" :key="index">
       <g-link :to="post.node.path">{{ post.node.title }}</g-link>
+      <p class="timestamp">posted on : {{  new Date(post.node.date).toDateString() }}</p>
     </div>
   </Layout>
 </template>
@@ -25,11 +28,10 @@
     archivedBlogs: allArchivedBlogs(order: ASC){
       edges {
         node {
-          id
           title
-          date (format: "D MMMM, YYYY")
           categories
           path
+          date
         }
       }
     }
@@ -38,6 +40,7 @@
         node{
           title
           path
+          published_at
         }
       }
     }
@@ -46,6 +49,7 @@
         node{
           title
           url
+          published_at
         }
       }
     }
@@ -65,7 +69,14 @@ export default {
 </script>
 
 <style scoped>
-.blog > ul {
-  list-style: none;
-}
+
+  .timestamp {
+    font-size: 0.8em;
+    color: grey;
+  }
+
+  .post {
+    padding-top: 10px;
+    line-height: 16px;
+  }
 </style>
