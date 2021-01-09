6b09e82d5de460daf0869e6fa71c9310356fdbfa
Alex Chiu <alexchiu11@gmail.com>
2021-01-09 15:14:42 +0000

    updated code block styling to use prsim-tomorrow

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 
 import DefaultLayout from '~/layouts/Default.vue'
 import "@/assets/styles/main.scss"
-import 'prismjs/themes/prism.css'
+import "prismjs/themes/prism-tomorrow.css"
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
