8f21a1402cb966affd42673011ef5bb8dee2882d
Alex Chiu <alexchiu11@gmail.com>
2021-01-10 11:26:52 +0000

    added time to read

--- src/templates/DevToArticles.vue
+++ src/templates/DevToArticles.vue
@@ -7,8 +7,9 @@
       Posted on : {{ new Date($page.article.published_at).toDateString() }}
     </p>
     <p class="article__timestamp">
-      Last updated on : {{ new Date($page.article.edited_at).toDateString() }}
+      Updated on : {{ new Date($page.article.edited_at).toDateString() }}
     </p>
+    <p class="article__timestamp">{{ $page.article.time_to_read }} min read</p>
     <ul class="article__tag-container">
       <li class="tag-container__tag" v-for="(tag, index) in $page.article.tags">
         #{{ tag }}
@@ -21,7 +22,7 @@
 
     <p class="devto-link">
       <a :href="$page.article.url" target="_blank">
-       > View this blog over at DEV.to. Feel free to leave a comment.</a
+        > View this blog over at DEV.to. Feel free to leave a comment.</a
       >
     </p>
   </Layout>
@@ -46,6 +47,7 @@ export default {
       edited_at
       tags
       url
+      time_to_read
     }
   }
 </page-query>
