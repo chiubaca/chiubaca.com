5a5c9030ecae00906cade9905cd9ce2de72abbc1
Alex Chiu <alexchiu11@gmail.com>
2021-01-10 11:48:02 +0000

    Added Cloudflare Analytics

--- src/main.js
+++ src/main.js
@@ -8,4 +8,9 @@ import "prismjs/themes/prism-tomorrow.css"
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
+
+  head.script.push(
+    { src: "https://static.cloudflareinsights.com/beacon.min.js", "data-cf-beacon": `{'token': ${process.env.CF_TOKEN}}`, async: true, defer: true }
+  )
+
 }
